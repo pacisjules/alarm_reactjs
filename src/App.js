536b1db652f5741c044ddeb11fbad326165f24a0
Ishimwe Jules Pacis (Mediator) <51479761+pacisjules@users.noreply.github.com>
2022-07-05 02:09:30 +0200

    new updates about radio stream

--- src/App.js
+++ src/App.js
@@ -1,68 +1,153 @@
 import React, { Component } from "react";
 import './App.css';
+import { FiClock } from 'react-icons/fi';
 
+
+
+const umuziki = new Audio();
+umuziki.src = "https://node-18.zeno.fm/3ncz6h98qrhvv?zs=kUq6enZQRI6vWUznFf7iOA&rj-tok=AAABgU63ytEAYqDDS9Hnk8GTjQ&rj-ttl=5";
 
 class App extends Component {
 
-  state={
-    number:1,
-    period:"..",
-    message:"",
-    show:"",
-    set:"21:40:0"
+  state = {
+    number: 1,
+    period: "..",
+    message: "Time now playing Kiss FM Rwanda stream....",
+    show: "",
+    setH: "24",
+    setM: "00",
+    setS: "0",
+    setted: 1,
+    txtDis: "",
+    me: "SET ALARM",
+    btncolor: "#000224",
+    play: 0,
   }
 
 
-  componentDidMount(){
-    
-    setInterval(()=>{
-      var date=new Date();
-      var hour=date.getHours();
-      var fullclock=date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
-      
+  play = () => {
+
+    if (this.state.play === 1) {
+      umuziki.play();
       this.setState({
-        number:date.getHours()+":"+date.getMinutes()+":"+date.getSeconds()
+        play: 0,
+      })
+    }
+  }
+
+
+  stop = () => {
+    umuziki.pause();
+    this.setState({
+      play: 0,
+      show: "",
+    })
+  }
+
+
+
+  componentDidMount() {
+
+    setInterval(() => {
+      var date = new Date();
+      var hour = date.getHours();
+      var fullclock = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+      var timeSetted = this.state.setH + ":" + this.state.setM + ":" + this.state.setS;
+
+
+
+      this.setState({
+        number: date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds()
       })
 
-      if(hour>=0&&hour<=12){
+      if (hour >= 0 && hour <= 12) {
         this.setState({
-          period:"AM"
+          period: "AM"
         })
-      }else{
+      } else {
         this.setState({
-          period:"PM"
-        })
-      }
-
-      if (fullclock === this.state.set) {
-        this.setState({
-          show: this.state.message
+          period: "PM"
         })
       }
 
-    },500)
+      if (fullclock === timeSetted && this.state.setted === 1) {
+        umuziki.play();
+        this.setState({
+          show: this.state.message,
+          setted: 0,
+          txtDis: "",
+          btncolor: "#000224",
+          me: "SET ALARM",
+          setH: "24",
+          setM: "00",
+          setS: "0",
+          play: 1,
+        })
+      }
+    }, 500)
   }
 
   render() {
     return (
       <>
         <div className="container">
+
           <div className="akazu">
-            <p className="clock">{this.state.number} {this.state.period}</p><br/>
+            <p className="clock"><FiClock style={{
+              color: "#e534eb",
+              fontSize: "30px",
+              marginRight: "10px",
+              animation: "isaha 5s infinite"
+            }} />{this.state.number} {this.state.period}</p><br />
+
+
+
             <div className="messeger">
-              <input type={"text"} placeholder="Type a message" value={this.state.set} onChange={(e)=>{
+              <h3>Set alarm (Kiss FM Rwanda stream)</h3>
+              <br />
+              <div className="ibihe">
+                <input type={"number"} min={"0"} max={"23"} value={this.state.setH} onChange={(e) => {
+                  this.setState({
+                    setH: e.target.value
+                  })
+                }} disabled={this.state.txtDis} />:
+
+                <input type={"number"} min={"0"} max={"23"} value={this.state.setM} onChange={(e) => {
+                  this.setState({
+                    setM: e.target.value
+                  })
+                }} disabled={this.state.txtDis} />:
+
+                <input type={"number"} min={"0"} max={"23"} value={this.state.setS} onChange={(e) => {
+                  this.setState({
+                    setS: e.target.value
+                  })
+                }} disabled={this.state.txtDis} />
+              </div>
+
+              <br />
+
+              <button onClick={() => {
+                umuziki.pause();
                 this.setState({
-                  set:e.target.value
+                  setted: 1,
+                  txtDis: "disabled",
+                  me: "ALARM SETTED",
+                  btncolor: "#78004c"
                 })
-              }}/><br/>
-              <input type={"text"} value={this.state.message} placeholder="Type a message"onChange={(e)=>{
-                this.setState({
-                  message:e.target.value
-                })
-              }}/><br/>
-              <br/>
-              <br/>
+              }}
+                style={{
+                  backgroundColor: this.state.btncolor,
+                }}
+              >{this.state.me}</button>
+              <br />
+              <button onClick={this.stop} style={{
+                display: this.state.play === 1 ? "block" : "none",
+              }}>STOP RADIO</button>
+              <p>{this.state.info}</p>
               <p>{this.state.show}</p>
+              <br />
+              <br />
             </div>
           </div>
         </div>
